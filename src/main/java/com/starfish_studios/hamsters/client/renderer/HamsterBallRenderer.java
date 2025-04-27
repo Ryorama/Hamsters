5bc2f235e148cf5bebd79ed7a51c2644d9638d19
mrsterner <simon@sterner.dev>
2025-04-27 14:50:57 +0200

    transparency is easy B-)

--- src/main/java/com/starfish_studios/hamsters/client/renderer/HamsterBallRenderer.java
+++ src/main/java/com/starfish_studios/hamsters/client/renderer/HamsterBallRenderer.java
@@ -1,26 +1,33 @@
 package com.starfish_studios.hamsters.client.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
+import com.starfish_studios.hamsters.Hamsters;
 import com.starfish_studios.hamsters.client.model.HamsterBallModel;
-import com.starfish_studios.hamsters.client.renderer.layers.HamsterInBallLayer;
+import com.starfish_studios.hamsters.entities.Hamster;
 import com.starfish_studios.hamsters.entities.HamsterBall;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import org.jetbrains.annotations.NotNull;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 @Environment(EnvType.CLIENT)
 public class HamsterBallRenderer extends GeoEntityRenderer<HamsterBall> {
+
+    private final ResourceLocation HAMSTER_MODEL = Hamsters.id("geo/entity/hamster.geo.json");
 
     private final EntityRendererProvider.Context renderManager;
 
     public HamsterBallRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new HamsterBallModel());
         this.renderManager = renderManager;
-        this.addRenderLayer(new HamsterInBallLayer(this, renderManager));
     }
 
     @Override
@@ -33,5 +40,25 @@ public class HamsterBallRenderer extends GeoEntityRenderer<HamsterBall> {
     @Override
     public boolean shouldShowName(@NotNull HamsterBall animatable) {
         return false;
+    }
+
+    @Override
+    public void actuallyRender(PoseStack poseStack, HamsterBall animatable, BakedGeoModel model, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        if (animatable.getFirstPassenger() instanceof Hamster hamster) {
+            poseStack.pushPose();
+
+            GeoEntityRenderer<? super Hamster> renderer = (GeoEntityRenderer<? super Hamster>) renderManager.getEntityRenderDispatcher().getRenderer(hamster);
+
+            var type = renderer.getRenderType(hamster, renderer.getTextureLocation(hamster), bufferSource, partialTick);
+            var consumer = bufferSource.getBuffer(type);
+
+            renderer.reRender(renderer.getGeoModel().getBakedModel(HAMSTER_MODEL), poseStack, bufferSource, hamster, type, consumer, partialTick, packedLight, packedOverlay, 1f,1f, 1f, 1f);
+
+            poseStack.popPose();
+
+            bufferSource.getBuffer(renderType); //refresh, keep this
+        }
+
+        super.actuallyRender(poseStack, animatable, model, renderType, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
     }
 }
